a7966dfc8ddf3c0f7a33d9f0476351f2f066a405
jayn95 <jill.navarra@wvsu.edu.ph>
2023-03-08 08:05:54 +0800

    mama

---
+++ Activity_4/octahedron.py
@@ -0,0 +1,69 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from scipy.spatial import Delaunay
+
+import tensorflow as tf
+
+tf.compat.v1.disable_eager_execution()
+#
+def _plt_basic_object_(points):
+    """Plots a basic object, assuming its convex and not too complex"""
+
+    tri = Delaunay(points).convex_hull
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    S = ax.plot_trisurf(points[:,0], points[:,1], points[:,2], triangles=tri, shade=True,lw=0.5)
+
+    ax.set_xlim3d(-5, 5)
+    ax.set_ylim3d(-5, 5)
+    ax.set_zlim3d(-5, 5)
+
+    plt.show()
+
+#shows/give points for cube (manipulate second method if you want to make other objects)
+def _cube_(bottom_lower=(0, 0, 0), side_length=5):
+    """Create cube starting form the given bottom-lower point (lowest x, y, z values)"""
+    bottom_lower = np.array(bottom_lower)
+
+    points = np.vstack([
+        bottom_lower,
+        bottom_lower + [-3, -3, 0],
+        bottom_lower + [3, -3, 0],
+        bottom_lower + [3, 3, 0],
+        bottom_lower + [-3, 3, 0],
+        bottom_lower + [0, 0, 10],
+        bottom_lower + [0, 0, -10],
+        bottom_lower,
+    ])
+
+    return points
+
+def translate_obj(points, amount):
+    return tf.add(points, amount)
+
+#points = cube, amount = what you want to manipulate
+#tf.constant = numpy array, array, data type (float)
+#when calling method from tensoflow call with session, close session to avoid problems after calling
+
+init_cube_ = _cube_(side_length=3)
+points = tf.constant(init_cube_, dtype=tf.float32)
+
+# _plt_basic_object_(init_cube_)
+
+#Update the values here to move the cube around. x,y,z
+translation_amount = tf.constant([0, 0, 0], dtype=tf.float32)
+translated_object = translate_obj(points, translation_amount)
+
+with tf.compat.v1.Session() as session:
+    translated_cube = session.run(translated_object)
+
+def main():
+
+    _plt_basic_object_(translated_cube)
+
+if __name__=='__main__':
+    main()
