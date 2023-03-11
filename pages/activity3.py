083d8780682329668cab607f5760975c7e76e559
jayn95 <126637904+jayn95@users.noreply.github.com>
2023-03-11 23:37:15 +0800

    Update activity3.py

--- pages/activity3.py
+++ pages/activity3.py
@@ -55,7 +55,8 @@ def main():
 
     for p in picture:
         img_ = cv2.imread(f"{p}")
-#         img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
+        img_ = cv2.cvtColor(img_, cv2.COLOR_BGR2RGB)
+        st.image(img_)
         translation(img_)
         rotation(img_)
         scaling(img_)
