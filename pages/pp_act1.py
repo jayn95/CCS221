973f42b7cd7ea50a2f51a3b75de3606ae2bc5357
jayn95 <126637904+jayn95@users.noreply.github.com>
2023-03-12 11:59:57 +0800

    Update pp_act1.py

--- pages/pp_act1.py
+++ pages/pp_act1.py
@@ -12,7 +12,7 @@ def DDALine(x1, y1, x2, y2, color):
 
     for i in range(0, int(steps + 1)):
 #         fig = plt.plot(int(x1), int(y1), color)
-        fig, ax = plt.subplots(int(x1), int(y1), color)
+        fig, ax = plt.subplots()
         ax.plot(x1, y1)
         x1 += Xinc
         y1 += Yinc
