6f49e9d843816ad5b6e4e1f1b1923ee586a477cb
jayn95 <126637904+jayn95@users.noreply.github.com>
2023-03-12 20:24:21 +0800

    Update octahedron.py

--- pages/octahedron.py
+++ pages/octahedron.py
@@ -17,15 +17,22 @@ def _plt_basic_object_(points):
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(111, projection='3d')
     S = ax.plot_trisurf(points[:,0], points[:,1], points[:,2], triangles=tri, shade=True,lw=0.5)
-
+    
+    ax.set_zlabel("Z-Axis")
     ax.set_xlim3d(-5, 5)
     ax.set_ylim3d(-5, 5)
     ax.set_zlim3d(-5, 5)
 
-    plt.show()
+    #Naming the Axis
+    plt.xlabel("X-Axis")
+    plt.ylabel("Y-Axis")
+
+    #Graph title
+    st.title("Octahedron")
+    st.pyplot(fig)
 
 #shows/give points for cube (manipulate second method if you want to make other objects)
-def _cube_(bottom_lower=(0, 0, 0), side_length=5):
+def _octahedron_(bottom_lower=(0, 0, 0), side_length=5):
     """Create cube starting form the given bottom-lower point (lowest x, y, z values)"""
     bottom_lower = np.array(bottom_lower)
 
@@ -49,8 +56,8 @@ def translate_obj(points, amount):
 #tf.constant = numpy array, array, data type (float)
 #when calling method from tensoflow call with session, close session to avoid problems after calling
 
-init_cube_ = _cube_(side_length=3)
-points = tf.constant(init_cube_, dtype=tf.float32)
+init_octahedron_ = _octahedron_(side_length=3)
+points = tf.constant(init_ctahedron_, dtype=tf.float32)
 
 # _plt_basic_object_(init_cube_)
 
@@ -59,11 +66,12 @@ translation_amount = tf.constant([0, 0, 0], dtype=tf.float32)
 translated_object = translate_obj(points, translation_amount)
 
 with tf.compat.v1.Session() as session:
-    translated_cube = session.run(translated_object)
+    translated_octahedron = session.run(translated_object)
 
 def main():
 
-    _plt_basic_object_(translated_cube)
+    _plt_basic_object_(translated_octahedron)
 
 if __name__=='__main__':
-    main()
+    value = main()
+    st.write(value)
