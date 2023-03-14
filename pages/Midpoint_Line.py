f9666bb5fe955f967cba30a90a12b16069455596
jayn95 <126637904+jayn95@users.noreply.github.com>
2023-03-14 13:40:44 +0800

    create midpoint_line.py

---
+++ pages/Midpoint_Line.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+
+def midpoint(x1, y1, x2, y2, color):
+    dx = x2 - x1
+    dy = y2 - y1
+
+   
+    d  = dy - (dx/2)
+    x = x1
+    y = y1
+
+    print(f"x = {x}, y = {y}")
+   
+    xcoordinates = [x]
+    ycoordinates = [y]
+
+    while (x<x2):
+        x = x + 1
+ 
+        if (d<0):
+            d = d + dy
+
+    
+        else:
+            d = d + (dy - dx)
+            y = y + 1
+
+        xcoordinates.append(x)
+        ycoordinates.append(y)
+        print(f"x = {x}, y = {y}")
+
+    xMid = (x1 + x2)/2
+    yMid = (y1 + y2)/2 
+    
+    plt.plot(xcoordinates, ycoordinates)
+    plt.plot(xMid, yMid, 'ro')
+    plt.show()
+
+def main():
+    x1 = st.number_input("Enter the Starting point of x: ")
+    y1 = st.number_input("Enter the Starting point of y: ")
+    x2 = st.number_input("Enter the end point of x: ")
+    y2 = st.number_input("Enter the end point of y: ")
+    color = ".r"
+
+    midpoint(x1, y1, x2, y2, color)
+
+
+if __name__== "__main__":
+    main()
