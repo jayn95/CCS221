f2b8d41e4c6f9f6d8677b2f195a5d0dd8f8255a0
jayn95 <126637904+jayn95@users.noreply.github.com>
2023-03-14 12:55:49 +0800

    Update pp_act1.py

--- pages/pp_act1.py
+++ pages/pp_act1.py
@@ -1,9 +1,16 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 
+st.title("Midpoint of DDA Line")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+
 def DDALine(x1, y1, x2, y2, color):
     dx = x2 - x1
     dy = y2 - y1
+    
+    xMid = (x1 + x2)/2
+    yMid = (y1 + y2)/2 
 
     steps = abs(dx) if abs(dx) > abs(dy) else abs(dy)
 
@@ -11,20 +18,21 @@ def DDALine(x1, y1, x2, y2, color):
     Yinc = float(dy / steps)
 
     for i in range(0, int(steps + 1)):
-        fig = plt.plot(int(x1), int(y1), color)
-#         fig, ax = plt.subplots()
-#         ax.plot(x1, y1)
+        plt.plot(int(x1), int(y1), color)
         x1 += Xinc
         y1 += Yinc
         
-#         return fig
+    plt.plot(xMid, yMid, 'bo')
+    st.pyplot()
+
+def main():
+    x1 = st.number_input("Enter the Starting point of x: ")
+    y1 = st.number_input("Enter the Starting point of y: ")
+    x2 = st.number_input("Enter the end point of x: ")
+    y2 = st.number_input("Enter the end point of y: ")
+    color = ".r"
     
-x1 = st.number_input("Enter the Starting point of x: ")
-y1 = st.number_input("Enter the Starting point of y: ")
-x2 = st.number_input("Enter the end point of x: ")
-y2 = st.number_input("Enter the end point of y: ")
-color = ".r"
+    DDALine(x1, y1, x2, y2, color)
 
-DDALine(x1, y1, x2, y2, color)
-
-st.pyplot()
+if __name__== "__main__":
+    main()
